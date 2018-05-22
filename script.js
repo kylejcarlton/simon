3d3f08abdfa8db8bd94ebbb2e71d5092c0495201
Kyle J Carlton <kylejcarlton@gmail.com>
2018-05-22 10:47:03 -0500

    UI

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
+  //https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
+  //https://s3.amazonaws.com/freecodecamp/simonSound2.mp3
+  //https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
+  //https://s3.amazonaws.com/freecodecamp/simonSound4.mp3.
 
 }); //END DOC READY
