67d03d29c4d1dc8b28cca42ac13967d5b4a8d99e
Kyle J Carlton <kylejcarlton@gmail.com>
2018-06-30 09:12:03 -0500

    initial game logic

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
   //https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
   //https://s3.amazonaws.com/freecodecamp/simonSound2.mp3
   //https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
-  //https://s3.amazonaws.com/freecodecamp/simonSound4.mp3.
+  //https://s3.amazonaws.com/freecodecamp/simonSound4.mp3
+
+$(document).ready(function(){
+
+  var gameOn = false;
+
+  $(".slider").click(function(){
+    if(gameOn == false){
+      gameOn = true;
+    }
+    else{
+      gameOn = false;
+    }
+  });
+
+  function powerSwitch(){
+
+  }
+
+
+
 
 }); //END DOC READY
