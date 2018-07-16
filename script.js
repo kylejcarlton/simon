dc81668dcd7172e5ed4431c9cd1e4aac4ccb6647
Kyle J Carlton <kylejcarlton@gmail.com>
2018-07-15 21:24:40 -0500

    finalize buttonPress logic

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
 
-  var isTouchDevice = 'ontouchstart' in document.documentElement, gameOn = false, strict = false, session = false, huTurn = true, game = [], buttons = [{"id": "#green","color0": "#2DA850","color1": "#2dda5e","sound": "document.getElementById('greenSound')"}, {"id": "#red","color0": "#960d16","color1": "#f02936","sound": "document.getElementById('redSound')"}, {"id": "#blue","color0": "#497ecc","color1": "#3d8bfe","sound": "document.getElementById('blueSound')"}, {"id": "#yellow","color0": "#c7a618","color1": "#f3cf30","sound": "document.getElementById('yellowSound')"}];
+  var isTouchDevice = 'ontouchstart' in document.documentElement, gameOn = true, session = true, huTurn = true, strict = false, game = [], buttons = [{"id": "green","color0": "2DA850","color1": "2dda5e","sound": "greenSound"}, {"id": "red","color0": "960d16","color1": "f02936","sound": "redSound"}, {"id": "#blue","color0": "#497ecc","color1": "#3d8bfe","sound": "document.getElementById('blueSound')"}, {"id": "#yellow","color0": "#c7a618","color1": "#f3cf30","sound": "document.getElementById('yellowSound')"}];
+
+  if(isTouchDevice){
+    $("#green").on('touchstart', function(){buttonPress(0, 1);})
+    .on('touchend', function(){buttonPress(0, 0);});
+    $("#red").on('touchstart', function(){buttonPress(1, 1);})
+    .on('touchend', function(){buttonPress(1, 0);});
+  }
+  else{
+    $("#green").mousedown(function(){buttonPress(0, 1);})
+    .mouseup(function(){buttonPress(0, 0);});
+    $("#red").mousedown(function(){buttonPress(1, 1);})
+    .mouseup(function(){buttonPress(1, 0);});
+  }
+
+  function buttonPress(which, state){
+    if(gameOn && session && huTurn == true){
+      colorState = "color"+state;
+      console.log(buttons[which].sound);
+      $("#" + buttons[which].id).css("background-color", "#"+buttons[which][colorState]);
+      if(state == 1){
+        document.getElementById(buttons[which].sound).play();
+      }
+    }
+  }
 
   $(".slider").click(function(){
     if(gameOn == false){
@@ -29,50 +53,5 @@ $(document).ready(function(){
   $("#start").click(function(){
     session = true;
   });
-
-  function buttonPress(which, state){
-    if(gameOn && session && huTurn == true){
-      console.log("which: "+which);
-      console.log("state: "+state);
-    }
-  }
- 
-  if(isTouchDevice){
-    $("#green").on('touchstart', function(){buttonPress(0, 0);})
-    .on('touchend', function(){buttonPress(0, 1);});
-  }
-  else{
-    $("#green").mousedown(function(){buttonPress(0, 0);})
-    .mouseup(function(){buttonPress(0, 1);});
-  }
-
-  $("#red")
-  .mousedown(function(){
-    $("#red").css("background-color", "#f02936");
-    buttonSounds[1].play();
-  })
-  .mouseup(function(){
-    $("#red").css("background-color", "#960d16");
-  });
-
-  $("#blue")
-  .mousedown(function(){
-    $("#blue").css("background-color", "#3d8bfe");
-    buttonSounds[2].play();
-  })
-  .mouseup(function(){
-    $("#blue").css("background-color", "#497ecc");
-  });
-
-  $("#yellow")
-  .mousedown(function(){
-    $("#yellow").css("background-color", "#f3cf30");
-    buttonSounds[3].play();
-  })
-  .mouseup(function(){
-    $("#yellow").css("background-color", "#c7a618");
-  });
-
-
 
 }); //END DOC READY
