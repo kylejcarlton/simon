206c42774531c223d499d95ee298ce793f61fc03
Kyle J Carlton <kylejcarlton@gmail.com>
2018-07-17 11:57:51 -0500

    add event listener for touch detection

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
 
-  var isTouchDevice = 'ontouchstart' in document.documentElement, gameOn = true, session = true, huTurn = true, strict = false, game = [], buttons = [{"id": "green","color0": "2DA850","color1": "2dda5e","sound": "greenSound"}, {"id": "red","color0": "960d16","color1": "f02936","sound": "redSound"}, {"id": "#blue","color0": "#497ecc","color1": "#3d8bfe","sound": "document.getElementById('blueSound')"}, {"id": "#yellow","color0": "#c7a618","color1": "#f3cf30","sound": "document.getElementById('yellowSound')"}];
+  var isTouchDevice = false, gameOn = true, session = true, huTurn = true, strict = false, game = [], buttons = [{"id": "green","color0": "2DA850","color1": "2dda5e","sound": "greenSound"}, {"id": "red","color0": "960d16","color1": "f02936","sound": "redSound"}, {"id": "blue","color0": "497ecc","color1": "3d8bfe","sound": "blueSound"}, {"id": "yellow","color0": "c7a618","color1": "f3cf30","sound": "yellowSound"}];
 
-  if(isTouchDevice){
+  window.addEventListener('touchstart', function(){
+    isTouchDevice = true;
+    touchDeviceDetect();
+  });
+
+  function touchDeviceDetect (){
     $("#green").on('touchstart', function(){buttonPress(0, 1);})
     .on('touchend', function(){buttonPress(0, 0);});
     $("#red").on('touchstart', function(){buttonPress(1, 1);})
     .on('touchend', function(){buttonPress(1, 0);});
+    $("#blue").on('touchstart', function(){buttonPress(2, 1);})
+    .on('touchend', function(){buttonPress(2, 0);});
+    $("#yellow").on('touchstart', function(){buttonPress(3, 1);})
+    .on('touchend', function(){buttonPress(3, 0);});
   }
-  else{
-    $("#green").mousedown(function(){buttonPress(0, 1);})
-    .mouseup(function(){buttonPress(0, 0);});
-    $("#red").mousedown(function(){buttonPress(1, 1);})
-    .mouseup(function(){buttonPress(1, 0);});
-  }
+  
+  $("#green").mousedown(function(){buttonPress(0, 1);})
+  .mouseup(function(){buttonPress(0, 0);});
+  $("#red").mousedown(function(){buttonPress(1, 1);})
+  .mouseup(function(){buttonPress(1, 0);});
+  $("#blue").mousedown(function(){buttonPress(2, 1);})
+  .mouseup(function(){buttonPress(2, 0);});
+  $("#yellow").mousedown(function(){buttonPress(3, 1);})
+  .mouseup(function(){buttonPress(3, 0);});
 
   function buttonPress(which, state){
     if(gameOn && session && huTurn == true){
       colorState = "color"+state;
-      console.log(buttons[which].sound);
       $("#" + buttons[which].id).css("background-color", "#"+buttons[which][colorState]);
       if(state == 1){
         document.getElementById(buttons[which].sound).play();
